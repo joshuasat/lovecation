34e136ff35c7b640d1d74d999c55a56c63134050
joshuasl12 <joshuasatyalaksana1@gmail.com>
2020-05-25 16:45:57 +0700

    responsive 5.0

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Fade from "react-reveal/Fade";
 
 import Button from "elements/Button";
 import BrandIcon from "parts/IconText";
 
 export default function Header(props) {
+  const [toggle, setToggle] = useState(false);
   const getNavLinkClass = (path) => {
     return props.location.pathname === path ? " active" : "";
   };
@@ -32,7 +33,8 @@ export default function Header(props) {
             <BrandIcon />
 
             <button
-              className="navbar-toggler"
+              onClick={() => setToggle(!toggle)}
+              className={`navbar-toggler${toggle === true ? "" : " collapsed"}`}
               type="button"
               data-toggle="collapse"
               data-target="#navbar-lovecation"
@@ -41,9 +43,14 @@ export default function Header(props) {
               aria-label="Toggle navigation"
               style={{ borderRadius: 5 }}
             >
-              <span className="navbar-toggler-icon"></span>
+              <span className="navbar-toggler-icon "></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbar-lovecation">
+            <div
+              className={`navbar-collapse collapse ${
+                toggle === true ? "show" : ""
+              }`}
+              id="navbar-lovecation"
+            >
               <ul className="navbar-nav ml-auto">
                 <li className={`nav-item${getNavLinkClass("/")}`}>
                   <Button className="nav-link" type="link" href="/">
